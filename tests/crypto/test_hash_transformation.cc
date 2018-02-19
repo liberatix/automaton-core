8ea01e9eacf36341af48907953372cf932140cc9
samir-github <Samir>
2018-02-19 17:48:55 +0200

    Fixed few compilation errors

--- tests/crypto/test_hash_transformation.cc
+++ tests/crypto/test_hash_transformation.cc
@@ -21,8 +21,8 @@ const char* DUMMY1 = "dummy1";
 const char* DUMMY2 = "dummy2";
 const char* DUMMY3 = "dummy3";
 
-const char * TEST1 = "abc";
-const char * TEST2 = "test";
+const unsigned char * TEST1 = (const unsigned char*)"abc";
+const unsigned char * TEST2 = (const unsigned char*)"test";
 
 // Tests hash transformation registration.
 TEST(HashTranformation, Registration) {
@@ -35,13 +35,13 @@ TEST(HashTranformation, Registration) {
 
   hash_transformation * d1 = hash_transformation::create(DUMMY1);
   EXPECT_NE(d1, nullptr);
-  d1->calculate_digest(TEST1, strlen(TEST1), &digest);
-  EXPECT_EQ(digest, 1);
+  d1->calculate_digest(TEST1, strlen((const char*)TEST1), digest);
+  EXPECT_EQ(digest[0], 1);
 
   hash_transformation * d2 = hash_transformation::create(DUMMY2);
   EXPECT_NE(d2, nullptr);
-  d2->calculate_digest(TEST1, strlen(TEST1), &digest);
-  EXPECT_EQ(digest, 2);
+  d2->calculate_digest(TEST1, strlen((const char*)TEST1), digest);
+  EXPECT_EQ(digest[0], 2);
 }
 
 // Tests hash transformation creation.
