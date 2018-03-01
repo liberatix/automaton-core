a6e674ac398bb9c723f00a37d822a83d88961121
samir-github <Samir>
2018-03-01 14:11:44 +0200

    Better dsa interface

---
+++ crypto/dsa.cc
@@ -0,0 +1,19 @@
+#include "dsa.h"
+
+std::map<std::string, dsa::dsa_factory_function> dsa::dsa_factory;
+
+dsa * dsa::create(std::string algorithm) {
+  auto it = dsa_factory.find(algorithm);
+  if (it == dsa_factory.end()) {
+    return nullptr;
+  }
+  else {
+    return it->second();
+  }
+}
+
+void dsa::register_factory(std::string name, dsa_factory_function func) {
+  dsa_factory[name] = func;
+}
+
+
