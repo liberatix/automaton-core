0426439cf4d280ba2b79d6cb049e2818deafdb13
liberatix-kari <kari@liberatix.com>
2018-03-09 14:29:33 +0200

    Schema interface. It can be used to create data schemas including messages, enums, nested messages and enums. Message fields can be of type message, enum, string ot int32. Serialization/deserialization to/from sting is supported.

---
+++ data/schema.cc
@@ -0,0 +1,10 @@
+#include "data/schema.h"
+
+schema::field_info::field_info(int tag, field_type type, const std::string&
+    name, const std::string& fully_qualified_type, bool is_repeated) {
+  this->tag = tag;
+  this->type = type;
+  this->name = name;
+  this->fully_qualified_type = fully_qualified_type;
+  this->is_repeated = is_repeated;
+}
