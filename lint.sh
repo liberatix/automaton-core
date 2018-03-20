61ce7539ceb86c46137adda9c6b91af2d53b0b0b
liberatix-ace <asen@liberatix.com>
2018-03-20 13:36:15 +0200

    Skip bazel-* paths from lint

--- lint.sh
+++ lint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -path ./third_party -prune -o -iname *.cc -o -iname *.cpp -o -iname *.h | xargs cpplint.py --filter=-legal/copyright,-build/header_guard,-build/c++11
+find . -path ./third_party -prune -o -path "./bazel-*" -prune -o -iname *.cc -o -iname *.cpp -o -iname *.h | xargs cpplint.py --filter=-legal/copyright,-build/header_guard,-build/c++11
