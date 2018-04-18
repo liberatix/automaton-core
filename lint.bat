86a0df0537720e1d37461ac629cfee588cf9a8ee
Martin Penkov <martin@penkov.me>
2018-04-18 18:32:38 +0300

    extracted the cpplint options as a common variable, removed extra call to directory "conditions"

--- lint.bat
+++ lint.bat
@@ -1,22 +1,23 @@
 set extentions=(*.cc,*.h)
+set options=--filter=-legal/copyright,-build/header_guard,-build/c++11
 
 set dir=%cd%\conditions
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\crypto
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\data
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\network
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\state
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\storage
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
 
 set dir=%cd%\tests
-for /r %dir% %%v in %extentions% do cpplint --filter=-legal/copyright,-build/header_guard,-build/c++11 %%v
+for /r %dir% %%v in %extentions% do cpplint %options% %%v
