48c39e06917c9981d708b7d6ba1b0dcafecb6d4f
liberatix-ace <asen@liberatix.com>
2018-05-02 18:57:25 +0300

    data -> schema, add protobuf to third_party
    Adding the build files using local repositories to avoid constant download from github

---
+++ tests/schema/protobuf_schema_test_find_all_fields.cc
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "schema/protobuf_schema.h"
+#include "gtest/gtest.h"
+
+std::string file_to_bytes(char const* filename) {
+  std::ifstream is(filename, std::ifstream::binary);
+  if (is) {
+    is.seekg(0, is.end);
+    int length = is.tellg();
+    is.seekg(0, is.beg);
+    char* buffer = new char[length];
+    std::cout << "Reading " << length << " characters... ";
+    is.read(buffer, length);
+    if (is) {
+      std::cout << "all characters read successfully." << std::endl;
+    } else {
+      std::cout << "error: only " << is.gcount() << " could be read"
+          << std::endl;
+    }
+    is.close();
+    return std::string(buffer, length);
+  }
+  return "";
+}
+
+TEST(protobuf_schema, find_all_fields) {
+  protobuf_schema sc;
+  sc.import_schema_from_string(file_to_bytes("tests/schema/many_fields.proto"),
+  "test", "");
+  int k;
+  int id  = sc.get_schema_id("TestMsg");
+  k = sc.get_fields_number(id);
+  EXPECT_EQ(k, 0);
+  id = sc.get_schema_id("TestMsg2");
+  k = sc.get_fields_number(id);
+  EXPECT_EQ(k, 7);
+  id  = sc.get_schema_id("TestMsg4.TestMsg5");
+  k = sc.get_fields_number(id);
+  EXPECT_EQ(k, 1);
+  id  = sc.get_schema_id("TestMsg4.TestMsg5.TestMsg6");
+  k = sc.get_fields_number(id);
+  EXPECT_EQ(k, 1);
+  google::protobuf::ShutdownProtobufLibrary();
+}
