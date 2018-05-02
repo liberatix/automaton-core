659b03e087745091a92e81d748888bc394de7cd4
Martin Penkov <martin@liberatix.com>
2018-05-02 12:15:27 +0300

    the basic hash miner has been properly encapsulated, the private variables now follow the google underscore guidelines

--- examples/crypto/basic_hash_miner.h
+++ examples/crypto/basic_hash_miner.h
@@ -4,12 +4,21 @@
 #include "crypto/hash_transformation.h"
 
 class basic_hash_miner {
- public:
-  int get_nonce_lenght();
-  explicit basic_hash_miner(hash_transformation* hash_transformation);
-  unsigned char* mine(unsigned char* block_hash,
-    int block_hash_lenght,
-    int required_leading_zeros);
+  public:
+    explicit basic_hash_miner(hash_transformation* hash_transformation);
+
+    int get_nonce_lenght();
+    unsigned char* mine(unsigned char* block_hash,
+      int block_hash_lenght,
+      int required_leading_zeros);
+
+  private:
+    int nonce_lenght_;
+    hash_transformation* hash_transformation_;
+    unsigned char* nonce_;
+
+    void next_nonce();
+    bool is_valid_next_block_hash(unsigned char* hash, int required_leading_zeros);
 };
 
 #endif  // AUTOMATON_CORE_EXAMPLES_SIMPLE_MINER__
