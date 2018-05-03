7e3fef5e6a9da4780ddcb7b95aa6fdf244666a37
liberatix-samir <36536055+liberatix-samir@users.noreply.github.com>
2018-05-03 13:55:35 +0300

    Fixes

--- crypto/Keccak_256_cryptopp.cc
+++ crypto/Keccak_256_cryptopp.cc
@@ -1,44 +1,38 @@
-#include "crypto/keccak_256_cryptopp.h"
+#include "crypto/RIPEMD160_cryptopp.h"
 #include "crypto/hash_transformation.h"
 #include "cryptlib.h"  // NOLINT
-#include "keccak.h"  // NOLINT
+#include "ripemd.h"  // NOLINT
 
-Keccak_256_cryptopp::Keccak_256_cryptopp() {
-  hash = new CryptoPP::Keccak_256;
+RIPEMD160_cryptopp::RIPEMD160_cryptopp() {
+  hash = new CryptoPP::RIPEMD160;
 }
 
-void Keccak_256_cryptopp::calculate_digest(const unsigned char * input,
-                                           const size_t length,
-                                           unsigned char * digest) {
-  if (length) {
-    hash->CalculateDigest(digest, input, length);
-  } else {
-    hash->Final(digest);
-  }
+void RIPEMD160_cryptopp::calculate_digest(const unsigned char * input,
+                                          const size_t length,
+                                          unsigned char * digest) {
+  hash->CalculateDigest(digest, length == 0 ? nullptr : input, length);
 }
 
-void Keccak_256_cryptopp::update(const unsigned char * input,
-                                 const size_t length) {
-  if (length) {
-    hash->Update(input, length);
-  }
+void RIPEMD160_cryptopp::update(const unsigned char * input,
+                                const size_t length) {
+  hash->Update(length == 0 ? nullptr : input, length);
 }
 
-void Keccak_256_cryptopp::final(unsigned char * digest) {
+void RIPEMD160_cryptopp::final(unsigned char * digest) {
   hash->Final(digest);
 }
 
-void Keccak_256_cryptopp::restart() {
+void RIPEMD160_cryptopp::restart() {
   hash->Restart();
 }
 
-unsigned int Keccak_256_cryptopp::digest_size() const {
+unsigned int RIPEMD160_cryptopp::digest_size() const {
   return _digest_size;
 }
 
-bool Keccak_256_cryptopp::register_self() {
-  hash_transformation::register_factory("Keccak_256",
-    [] {return reinterpret_cast<hash_transformation*>
-        (new Keccak_256_cryptopp()); });
+bool RIPEMD160_cryptopp::register_self() {
+  hash_transformation::register_factory("RIPEMD160",
+  [] {return reinterpret_cast<hash_transformation*>
+      (new RIPEMD160_cryptopp()); });
   return true;
 }
