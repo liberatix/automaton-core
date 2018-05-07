19eb5a0628cf859afa7f826d4a9386be5e843cb3
liberatix-ace <asen@liberatix.com>
2018-05-07 18:16:34 +0300

    Delete protobuf Message in destructor

--- schema/protobuf_schema_message.h
+++ schema/protobuf_schema_message.h
@@ -27,6 +27,12 @@ class protobuf_schema_message : public schema_message {
   */
   explicit protobuf_schema_message(google::protobuf::Message * m) : m(m) {}
 
+  ~protobuf_schema_message() {
+    if (m != NULL) {
+      delete m;
+    }
+  }
+
   operator google::protobuf::Message*() {
     return m;
   }
