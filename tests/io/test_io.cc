87cd5fe9849be20533acea2803ac97471b358d4e
liberatix-ace <asen@liberatix.com>
2018-05-08 16:49:14 +0300

    io utility library + tests

---
+++ tests/io/test_io.cc
@@ -0,0 +1,9 @@
+#include "io/io.h"
+#include "gtest/gtest.h"
+
+const char * TEST_TXT_CONTENTS = "Hello, World!";
+
+TEST(io, get_file_contents) {
+  std::string test_txt = get_file_contents("tests/io/test.txt");
+  EXPECT_EQ(test_txt, TEST_TXT_CONTENTS);
+}
