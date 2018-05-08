8d27e3a6c8842d10eb6c7caf661740873ad7ee03
liberatix-ace <asen@liberatix.com>
2018-05-08 18:25:31 +0300

    Make schema tests use new io & new schema interface

--- tests/schema/protobuf_schema_test.cc
+++ tests/schema/protobuf_schema_test.cc
@@ -2,31 +2,9 @@
 #include <iostream>
 #include <string>
 
+#include "io/io.h"
 #include "schema/protobuf_schema.h"
 #include "schema/protobuf_schema_definition.h"
-
-std::string file_to_bytes(char const* filename) {
-  std::ifstream is(filename, std::ifstream::binary);
-  if (is) {
-    is.seekg(0, is.end);
-    int length = is.tellg();
-    is.seekg(0, is.beg);
-    char* buffer = new char[length];
-    std::cout << "Reading " << length << " characters... ";
-    // read data as a block:
-    is.read(buffer, length);
-    if (is) {
-      std::cout << "all characters read successfully." << std::endl;
-    } else {
-      std::cout << "error: only " << is.gcount() << " could be read"
-          << std::endl;
-    }
-    is.close();
-    return std::string(buffer, length);
-  }
-  return "";
-  // Error reading proto file
-}
 
 int main(int argc, char* argv[]) {
   try {
@@ -55,8 +33,8 @@ int main(int argc, char* argv[]) {
     custom_schema2.add_message(m2);
 
     protobuf_schema sc;
-    sc.import_schema_from_string(file_to_bytes("tests/data/test.proto"),
-      "name1", "pack1");
+    sc.import_schema_from_string(
+        get_file_contents("tests/data/test.proto"), "name1", "pack1");
     sc.import_schema_definition(&custom_schema2, "name2", "pack2");
     sc.import_schema_definition(&custom_schema, "name3", "pack3");
     for (int i = 0; i < sc.get_schemas_number(); i++) {
--- tests/schema/protobuf_schema_test_message_serialization.cc
+++ tests/schema/protobuf_schema_test_message_serialization.cc
@@ -24,20 +24,22 @@ TEST(protobuf_schema, message_serialization) {
   protobuf_schema sc;
   sc.import_schema_definition(&custom_schema, "test", "");
 
-  int id1 = sc.new_message(0);
-  int id2 = sc.new_message(0);
+  schema_message * msg1 = sc.new_message(0);
+  schema_message * msg2 = sc.new_message(0);
 
-  sc.set_string(id1, 1, "value");
-  sc.set_repeated_int32(id1, 2, 7, -1);
-  sc.set_repeated_int32(id1, 2, 11, -1);
+  msg1->set_string(1, "value");
+  msg1->set_repeated_int32(2, 7, -1);
+  msg1->set_repeated_int32(2, 11, -1);
 
-  std::string message;
-  sc.serialize_message(id1, &message);
-  sc.deserialize_message(id2, message);
-  EXPECT_EQ(sc.get_string(id1, 1), "value");
-  EXPECT_EQ(sc.get_string(id2, 1), sc.get_string(id1, 1));
-  EXPECT_EQ(sc.get_repeated_field_size(id2, 2), 2);
-  EXPECT_EQ(sc.get_repeated_int32(id2, 2, 0), 7);
-  EXPECT_EQ(sc.get_repeated_int32(id2, 2, 1), 11);
+  std::string data;
+  msg1->serialize_message(&data);
+  msg2->deserialize_message(data);
+
+  EXPECT_EQ(msg1->get_string(1), "value");
+  EXPECT_EQ(msg2->get_string(1), msg1->get_string(1));
+  EXPECT_EQ(msg2->get_repeated_field_size(2), 2);
+  EXPECT_EQ(msg2->get_repeated_int32(2, 0), 7);
+  EXPECT_EQ(msg2->get_repeated_int32(2, 1), 11);
+
   google::protobuf::ShutdownProtobufLibrary();
 }
