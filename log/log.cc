34c7f9a954a0c73ca95e62ba8889df2c0db5d9eb
liberatix-ace <asen@liberatix.com>
2018-05-18 05:06:40 +0300

    Logger default setup in a static block

--- log/log.cc
+++ log/log.cc
@@ -1,3 +1,23 @@
 #include "log/log.h"
 
+#include <string>
+
 INITIALIZE_EASYLOGGINGPP
+
+namespace log {
+
+bool init_logger() {
+  el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format,
+      std::string("%datetime %levshort [%logger] [%fbase:%line]: %msg"));
+
+  el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
+
+  el::Loggers::setLoggingLevel(el::Level::Global);
+  el::Loggers::setVerboseLevel(9);
+
+  return true;
+}
+
+bool _init_logger = init_logger();
+
+}  // namespace log
