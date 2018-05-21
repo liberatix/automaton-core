ad5ddaf75878c0a4ce510e3c87292b3807b78abd
liberatix-ace <asen@liberatix.com>
2018-05-21 18:03:14 +0300

    Add annotations to the DI test

--- tests/third_party/di/test_di.cc
+++ tests/third_party/di/test_di.cc
@@ -74,3 +74,61 @@ TEST(test_di, dynamic_bindings) {
 }
 
 }  // namespace dynamic_bindings
+
+// ANNOTATIONS
+namespace annotations {
+
+auto int_1 = [] {};
+struct int_2_t {
+} int_2;
+
+struct annotations1 {
+  /*<<Constructor with named parameters of the same `int` type>>*/
+  BOOST_DI_INJECT(
+      annotations1,
+      (named = int_1) int i1,
+      (named = int_2) int i2,
+      int i3) : i1(i1), i2(i2), i3(i3) {}
+
+  int i1 = 0;
+  int i2 = 0;
+  int i3 = 0;
+};
+
+struct annotations2 {
+  /*<<Constructor with named parameters of the same `int` type>>*/
+  BOOST_DI_INJECT(
+      annotations2,
+      (named = int_1) int i1,
+      (named = int_2) int i2,
+      int i3) {}
+
+ private:
+  int i1 = 0;
+  int i2 = 0;
+  int i3 = 0;
+};
+
+
+TEST(boost_di, annotations) {
+  {
+    /*<<make injector and bind named parameters>>*/
+    auto injector = di::make_injector(
+      di::bind<int>().named(int_1).to(42)
+    , di::bind<int>().named(int_2).to(87)
+    , di::bind<int>().to(123));
+
+    /*<<create `annotations`>>*/
+    auto a1 = injector.create<annotations1>();
+    ASSERT_EQ(a1.i1, 42);
+    ASSERT_EQ(a1.i2, 87);
+    ASSERT_EQ(a1.i3, 123);
+
+    auto a2 = injector.create<annotations2>();
+    ASSERT_EQ(a2.i1, 42);
+    ASSERT_EQ(a2.i2, 87);
+    ASSERT_EQ(a2.i3, 123);
+  }
+}
+
+}  // namespace annotations
