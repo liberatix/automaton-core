7cfb266628683ba80e670a4a57967c49e7457055
liberatix-ace <asen@liberatix.com>
2018-06-12 11:51:25 +0300

    Add non 0 length hash test for script created object instances

--- src/automaton/tests/script/test_script.cc
+++ src/automaton/tests/script/test_script.cc
@@ -68,12 +68,13 @@ TEST_F(test_script, module_registration) {
 
   std::string tests[][3] = {
     {"keccak256", "", "C5D2460186F7233C927E7DB2DCC703C0E500B653CA82273B7BFAD8045D85A470"},
+    {"keccak256", "abc", "4E03657AEA45A94FC7D47BA826C8D667C0D1E6E33A64A036EC44F58FA12D6C45"},
     {"ripemd160", "", "9C1185A5C5E9FC54612808977EE8F548B2258D31"},
     {"sha256", "", "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"},
     {"sha3", "", "A7FFC6F8BF1ED76651C14756A061D662F580FF4DE43B49FA82D80A4B80F8434A"},
     {"sha512", "",
-     "CF83E1357EEFB8BDF1542850D66D8007D620E4050B5715DC83F4A921D36CE9CE"
-     "47D0D13C5D85F2B0FF8318D2877EEC2F63B931BD47417A81A538327AF927DA3E"},
+        "CF83E1357EEFB8BDF1542850D66D8007D620E4050B5715DC83F4A921D36CE9CE"
+        "47D0D13C5D85F2B0FF8318D2877EEC2F63B931BD47417A81A538327AF927DA3E"},
   };
 
   // Test instantiation and correctness of dynamically created hash_transformation objects.
@@ -82,7 +83,7 @@ TEST_F(test_script, module_registration) {
         dynamic_cast<crypto::hash_transformation*>(create_by_name("cryptopp.v0." + test[0]));
 
     hash->calculate_digest(
-        reinterpret_cast<const uint8_t*>(test[1].c_str()), tests[1].length(), &digest[0]);
+        reinterpret_cast<const uint8_t*>(test[1].c_str()), test[1].length(), &digest[0]);
     EXPECT_EQ(toHex(digest, hash->digest_size()), test[2]);
   }
 
