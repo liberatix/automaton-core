4f32e40226740732155ebd6330dc4bd40dcbfbaa
liberatix-kari <36536093+liberatix-kari@users.noreply.github.com>
2018-08-07 18:15:02 +0300

    Node interface (#242)
    * node interface
    
    * fix lint error
    
    * node functions empty impl
    
    * fixed linker errors

--- src/automaton/core/smartproto/node.cc
+++ src/automaton/core/smartproto/node.cc
@@ -21,6 +21,61 @@ node::node(unique_ptr<data::schema> schema, const string& lua_script)
       lua.safe_script(lua_script, &sol::script_pass_on_error);
 }
 
+peer_info::peer_info() {}
+
+peer_info node::get_peer_info(peer_id id) {
+  return peer_info();
+}
+
+void node::send_message(peer_id id, const core::data::msg& message) {}
+
+bool node::connect(peer_id id) {
+  return false;
+}
+
+bool node::disconnect(peer_id id) {
+  return false;
+}
+
+bool node::set_acceptor(const char* address) {
+  return false;
+}
+
+peer_id node::add_peer(const char* address) {
+  return 0;
+}
+
+void node::remove_peer(peer_id id) {}
+
+std::vector<peer_id> node::list_known_peers() {
+  return std::vector<peer_id> ();
+}
+
+std::vector<peer_id> node::list_connected_peers() {
+  return std::vector<peer_id> ();
+}
+
+void node::on_message_received(core::network::connection* c, char* buffer,
+    uint32_t bytes_read, uint32_t id) {}
+
+void node::on_message_sent(core::network::connection* c, uint32_t id,
+    core::network::connection::error e) {}
+
+void node::on_connected(core::network::connection* c) {}
+
+void node::on_disconnected(core::network::connection* c) {}
+
+void node::on_error(core::network::connection* c, core::network::connection::error e) {}
+
+bool node::on_requested(core::network::acceptor* a, const std::string& address) {
+  return false;
+}
+
+void node::on_connected(core::network::acceptor* a, core::network::connection* c,
+    const std::string& address) {}
+
+void node::on_error(core::network::acceptor* a, core::network::connection::error e) {}
+
 }  // namespace smartproto
 }  // namespace core
 }  // namespace automaton
--- src/automaton/core/smartproto/node.h
+++ src/automaton/core/smartproto/node.h
@@ -3,31 +3,82 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "automaton/core/data/factory.h"
 #include "automaton/core/data/msg.h"
 #include "automaton/core/data/schema.h"
+#include "automaton/core/network/acceptor.h"
+#include "automaton/core/network/connection.h"
 #include "automaton/core/script/lua/lua_script_engine.h"
+
+typedef uint32_t peer_id;
 
 namespace automaton {
 namespace core {
 namespace smartproto {
 
-struct peer {};
-struct peer_info {};
+struct peer_info {
+  peer_id id;
+  std::string address;
+  core::network::connection::state status;
 
-class node {
+  peer_info();
+};
+
+class node: public core::network::connection::connection_handler,
+    public core::network::acceptor::acceptor_handler {
  public:
-  node(std::unique_ptr<data::schema> schema,
-       const std::string& lua_script);
+  node(std::unique_ptr<data::schema> schema, const std::string& lua_script);
+
+  peer_info get_peer_info(peer_id id);
+
+  void send_message(peer_id id, const core::data::msg& message);
+
+  bool connect(peer_id id);
+
+  bool disconnect(peer_id id);
+
+  bool set_acceptor(const char* address);
+
+  peer_id add_peer(const char* address);
+
+  void remove_peer(peer_id id);
+
+  std::vector<peer_id> list_known_peers();
+
+  std::vector<peer_id> list_connected_peers();
 
  private:
   script::lua::lua_script_engine script_engine;
-  std::vector<peer> connected_peers;
-  std::vector<peer_info> known_peers;
   std::unique_ptr<data::factory> msg_factory;
   std::unique_ptr<data::schema> schema;
+  core::network::acceptor* acceptor_;
+  std::mutex peers_mutex;
+  std::unordered_map<peer_id, peer_info> known_peers;
+  std::unordered_map<peer_id, core::network::connection*> connected_peers;
+
+  // Inherited handlers' functions
+
+  void on_message_received(core::network::connection* c, char* buffer,
+      uint32_t bytes_read, uint32_t id);
+
+  void on_message_sent(core::network::connection* c, uint32_t id,
+      core::network::connection::error e);
+
+  void on_connected(core::network::connection* c);
+
+  void on_disconnected(core::network::connection* c);
+
+  void on_error(core::network::connection* c, core::network::connection::error e);
+
+  bool on_requested(core::network::acceptor* a, const std::string& address);
+
+  void on_connected(core::network::acceptor* a, core::network::connection* c,
+      const std::string& address);
+
+  void on_error(core::network::acceptor* a, core::network::connection::error e);
 };
 
 }  // namespace smartproto
