f79cab2038ebefe685684d79105728a131e5f0b0
liberatix-samir <Samir>
2018-08-10 18:53:19 +0300

    Updating the blockchain smart proto example

--- src/automaton/examples/smartproto/blockchain/blockchain.lua
+++ src/automaton/examples/smartproto/blockchain/blockchain.lua
@@ -59,29 +59,42 @@ function init()
 end
 
 
--- validate and add the block if we have it
+-- Valid block is a block that:
+-- 1. Is a new block, with valid hash and height >= 1
+-- 2. Either prev_hash is in blocks or the block is with height #1 and prev_hash is GENESIS_HASH
+-- 3. Has height difference of one with the blocks[prev_hash].height
+--    or it is block#1 with prev_hash equal to GENESIS_HASH
 function validateBlock(block)
-  -- get the block hash
+  -- get the block hash and target difficulty
   hash = blockHash(block)
+  local target = get_target(difficulty)
+
   print("validating block with hash: " .. hex(hash))
+  print("And height: " .. block.height)
+  print(block)
 
   -- Check if we already have the block
   if blocks[hash] ~= nil then
-    --print (BLOCK.DUPLICATE)
     return BLOCK.DUPLICATE
-  end
-  -- Check if we have the previous block
-  if block.prev_hash ~= GENESIS_HASH and blocks[block.prev_hash] == nil then
-    -- TODO(Samir): Currenly ignoring blocks with no parent, we will not sync if we are out of sync
-    return BLOCK.NO_PARENT
-  end
-  -- TODO(Samir): Check if hash is less than difficulty
-  local target = get_target(difficulty)
-  if hash > target then
+  -- Check if hash is greater than difficulty
+  elseif hash > target then
     print "In validateBlock, target > hash"
     return BLOCK.INVALID
+  -- block height can't be less than one
+  elseif block.height < 1 then
+    return BLOCK.INVALID
+  -- The block should have its predecessor in blocks unless it is the first block
+  elseif block.prev_hash ~= GENESIS_HASH and blocks[block.prev_hash] == nil then
+    return BLOCK.NO_PARENT
+  -- 1. If this is the first block, it needs to have GENESIS_HASH as prev_hash.
+  -- 2. If it is not the first block, check if the height of
+  --    the block with hash prev_hash is the height of this
+  elseif block.height == 1 and block.prev_hash ~= GENESIS_HASH
+      or block.height > 1 and blocks[block.prev_hash].height ~= block.height-1 then
+    return BLOCK.INVALID
+  else
+    return BLOCK.VALID
   end
-  return BLOCK.VALID
 end
 
 function onConnect(peer_id)
@@ -97,32 +110,41 @@ end
 
 function onBlock(peer_id, block)
   local block_validity = validateBlock(block)
-  -- TODO(Samir): Instead of checking block validity handle the cases
+  local hash = blockHash(block)
   print("Block Validity: " .. block_validity)
-  -- check if we have it
-  -- validate block
-  -- if valid, shout
-
-  --===========================
-
-    -- Is the block part of the longest chain?
-    if block.height == #blockchain then
-
-    --if block.prev_hash == "" and #blockchain == 0 then
-    --  blockchain[#blockchain] = block
-    --  blocks[hash] = block
-    --else
-
-    --else if block.prev_hash == blockchain[block.height - 1] then
-    end
-    -- get block hash
-
-    -- block is valid let's add it
+  
+  if block_validity == BLOCK.VALID  then
+    print "Valid block added to blocks"
     blocks[hash] = block
-
+    -- Check if we get a longer chain. Does not matter if it is the main or alternative.
+    if block.height == #blockchain+1 then
+      -- We are sure that this is the head of the longest chain
+      blockchain[#blockchain+1] = hash
+      -- Check if blocks[block.prev_hash] is part of the main chain and replace if necesery
+      local block_index = #blockchain-1
+      local longest_chain_hash = block.prev_hash
+      while block_index >= 1 and blockchain[block_index] ~= longest_chain_hash do
+        blockchain[block_index] = longest_chain_hash
+        block_index = block_index - 1
+        longest_chain_hash = blocks[longest_chain_hash].prev_hash
+      end
+    end
+    -- shout
+  -- If it is DUPLICATE
+  elseif(block_validity == BLOCK.DUPLICATE) then
+    -- do nothing
+  -- If it is INVALID
+  elseif(block_validity == BLOCK.INVALID) then
+    -- do nothing or respond with invalid
+  -- if there is no block with prev_hash in blocks
+  elseif(block_validity == BLOCK.NO_PARENT) then
+    -- potencial fork, the block will not be saved
+    -- find out which blocks we need to request or just request from start
+  end
 end
 
 function onBlocks(peer_id, msg)
+
 end
 
 function onGetBlocks(peer_id, msg)
@@ -130,7 +152,6 @@ function onGetBlocks(peer_id, msg)
 end
 
 -- Takes in block with miner, prev_hash, height
---
 function mine(miner, prev_hash, height, nonce, attempts)
   print "Mining block"
   local target = get_target(difficulty)
@@ -138,8 +159,6 @@ function mine(miner, prev_hash, height, nonce, attempts)
   for i = 0, attempts do
     block_hash = sha3(block_data .. nonce_str(nonce))
     if block_hash <= target then
-      -- create and return block
-      --print(hex(block_hash))
       -- create and return block
       mined_block = Block()
       mined_block.miner = miner
@@ -166,8 +185,10 @@ while i < 1 do
   local nonce = {0}
   target = get_target(difficulty)
   print(target)
-  local prev_hash = blockchain[#blockchain-1] or GENESIS_HASH
-  local found, block = mine(sha3("Samir"), prev_hash, #blockchain, nonce, 1000)
+  local prev_hash = blockchain[#blockchain] or GENESIS_HASH
+  --TODO(Samir): put miner, perv_hash, #blockchain+1 and nonce in a struct
+  --             and just previous BLOCK hash instead
+  local found, block = mine(sha3("Samir"), prev_hash, #blockchain+1, nonce, 1000)
   -- if a block is mined call broadcast to all peers
   if found then
     print(block)
@@ -191,83 +212,3 @@ function tests()
   --blocks[blockHash(genesis)] = genesis
   validateBlock(genesis)
 end
-
-
-
-
-
--- ===============================================================================================
--- ===============================================================================================
--- ===============================================================================================
-
-ITERATIONS = 1000000
-
--- for i = 1, 65537 do
---   inc_nonce(nonce)
---   print(hex(nonce_str(nonce)))
--- end
---
--- t = os.clock()
---
--- for i = 1, ITERATIONS do
---   inc_nonce(nonce)
--- end
--- print(hex(nonce_str(nonce)))
---
--- t = os.clock() - t
--- print(string.format("nonce inc [%.3f M/s]", ITERATIONS / t / 1000000))
---
---
--- -- Node initialization
--- --function init()
--- --end
---
--- function onBlockHeader(sender, msg)
---   hash = msg.hash
---   sender.blocks[msg.hash] = msg
---
---   if sender.blocks[msg.prev_hash] == nil then
---     -- Since we don't know anything about prior block from this peer
---     sender:send(GetHeaders({hash=msg.prev_hash, count=10}))
---   else
---
---   end
---
---   for i = 1,#peers do
---     peer = peers[i]
---     if peer.height < msg.height then
---       peer.send(msg)
---     end
---   end
--- end
---
--- print("Loading Blockchain Smart Protocol...")
--- x = 5
--- print(hex(sha3(abc)))
---
--- blocks = {}
---
--- m = Block()
---
--- m.miner = "test"
---
--- print(hex(m:serialize()))
---
--- t = os.clock()
--- for i = 1, ITERATIONS do
---   m.miner = i
--- --  blocks[#blocks + 1] = m:serialize()
--- end
--- t = os.clock() - t
--- print(string.format("create/set_name/serialize [%.3f M/s]", ITERATIONS / t / 1000000))
---
--- t = os.clock()
--- for i = 1, ITERATIONS do
---   m:set_blob(1, i)
--- --  blocks[#blocks + 1] = m:serialize()
--- end
--- t = os.clock() - t
--- print(string.format("create/set_id/serialize [%.3f M/s]", ITERATIONS / t / 1000000))
---
--- print(m.miner)
--- print(#blocks)
