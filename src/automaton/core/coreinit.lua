d964b22fc1888f903b006da8d7d551608d413587
liberatix-ace <36509087+liberatix-ace@users.noreply.github.com>
2018-08-10 16:47:46 +0300

    Logging + deadlock fix (#267)

--- src/automaton/core/coreinit.lua
+++ src/automaton/core/coreinit.lua
@@ -4,6 +4,33 @@ a2 = "127.0.0.1:5002"
 -- Google address
 -- a1 = "69.172.200.235:80"
 
+n = {}
+N = 50
+M = 8;
+
+function addr(i)
+  return "127.0.0.1:" .. tostring(i)
+end
+
+for i = 5000,5000 + N do
+  n[i] = anode()
+  print(addr(i))
+  n[i]:listen(addr(i))
+end
+
+for i = 5000,5000 + N do
+  for j = 1, M do
+    a = 5000 + ((i + M) - 5000) % N
+    print(i, a)
+    n[i]:add_peer(addr(a))
+    n[i]:connect(addr(a))
+  end 
+end
+
+p = n[5000]:peers()
+print("Connected peers " .. #p)
+
+--[[
 n1 = anode()
 n2 = anode()
 
@@ -26,3 +53,4 @@ m = n1:new_msg(3)
 m.f1 = 1
 m.f1 = 2
 m.f1 = 3
+]]
