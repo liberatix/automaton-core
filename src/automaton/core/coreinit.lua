b6885999565b7181ab4efab6a5e60fb106ac25ce
liberatix-kari <you@example.com>
2018-08-16 18:58:14 +0300

    test node with simulated connections

--- src/automaton/core/coreinit.lua
+++ src/automaton/core/coreinit.lua
@@ -1,6 +1,9 @@
-a1 = "tcp://127.0.0.1:5001"
-a2 = "tcp://127.0.0.1:5002"
+print("================== SIMULATION ==================")
 
+a1 = "sim://5:5:1"
+a2 = "sim://5:13:2"
+
+addr = "sim://150:1000:4:1"
 -- Google address
 -- a1 = "69.172.200.235:80"
 
@@ -37,8 +40,8 @@ n2 = anode()
 n1:listen(a1)
 n2:listen(a2)
 
-p2 = n1:add_peer(a2)
+p2 = n1:add_peer(addr)
+
 n1:connect(p2)
 
--- p1 = n2:add_peer(a1)
--- n2:connect(p1)
+n1:sendb(p2, "alabala", 1)
