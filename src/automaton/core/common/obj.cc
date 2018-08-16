e1ba6556d77a57d9f2006b668bf10f17678065d6
liberatix-ace <asen@liberatix.com>
2018-08-16 15:10:54 +0300

    Get rid of common and obj::process(...) requirement

--- src/automaton/core/common/obj.cc
+++
@@ -1,9 +0,0 @@
-#include "automaton/core/common/obj.h"
-
-namespace automaton {
-namespace core {
-namespace common {
-
-}  // namespace common
-}  // namespace core
-}  // namespace automaton
--- src/automaton/core/crypto/digital_signature.cc
+++ src/automaton/core/crypto/digital_signature.cc
@@ -9,30 +9,6 @@ namespace automaton {
 namespace core {
 namespace crypto {
 
-common::status digital_signature::process(const obj& request, obj* response) {
-  const msg* request_msg = dynamic_cast<const msg*>(&request);
-  auto request_msg_id = request_msg->get_schema_id();
-  auto request_msg_type = request_msg->get_message_type();
-
-  if (request_msg_type == "crypto.v0.dsig.sign.request") {
-    auto private_key = request_msg->get_blob(1);
-    auto message = request_msg->get_blob(2);
-    uint8_t * signature = new uint8_t[signature_size()];
-    if (response != nullptr) {
-      msg* response_msg = dynamic_cast<msg*>(response);
-      sign(reinterpret_cast<const uint8_t*>(private_key.data()),
-           reinterpret_cast<const uint8_t*>(message.data()),
-           message.size(),
-           signature);
-      response_msg->set_blob(1, std::string(reinterpret_cast<char*>(signature), signature_size()));
-    }
-    delete[] signature;
-    return common::status::ok();
-  }
-
-  return common::status::unimplemented(request_msg_type + " not implemented!");
-}
-
 }  // namespace crypto
 }  // namespace core
 }  // namespace automaton
