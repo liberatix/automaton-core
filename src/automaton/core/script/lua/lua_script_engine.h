ce2dd56f6d433711ac79cd7297d04becf2fcd99d
liberatix-ace <asen@liberatix.com>
2018-08-16 15:20:25 +0300

    Cleanup lua_script_engine

--- src/automaton/core/script/lua/lua_script_engine.h
+++ src/automaton/core/script/lua/lua_script_engine.h
@@ -44,26 +44,10 @@ class lua_script_engine {
 
   void bind_state();
 
-  /**
-  */
-  void bind_registered_modules();
-
-  /**
-  */
-  common::status execute(std::string script);
-
   auto get_lua_state() { return L; }
   auto& get_sol() { return lua; }
 
  private:
-  void bind_static_function(module* m,
-                            const module::static_function_info& func);
-
-  void bind_class(module* m,
-                  const module::implementation_info& info);
-
-  void bind_registered_module(module* m);
-
   lua_State* L;
   sol::state lua;
 
