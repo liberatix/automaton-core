a69bd5d8a2200847adaf2c2902c4c8216b1603ce
liberatix-ace <asen@liberatix.com>
2018-08-21 13:22:03 +0300

    ZMQ skeleton

---
+++ src/automaton/core/network/zmq_connection.h
@@ -0,0 +1,20 @@
+#ifndef AUTOMATON_CORE_NETWORK_ZMQ_IMPLEMENTATION_H_
+#define AUTOMATON_CORE_NETWORK_ZMQ_IMPLEMENTATION_H_
+
+#include "automaton/core/network/connection.h"
+#include "automaton/core/network/acceptor.h"
+
+namespace automaton {
+namespace core {
+namespace network {
+
+
+class zmq_connection: public connection {
+  
+};
+
+}  // namespace network
+}  // namespace core
+}  // namespace automaton
+
+#endif  // AUTOMATON_CORE_NETWORK_ZMQ_IMPLEMENTATION_H_
