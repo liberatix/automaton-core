5ad64f84e53366dd1019248aed324295ab20ca35
liberatix-ace <asen@liberatix.com>
2018-08-24 11:41:01 +0300

    Keep optimization in

--- build-and-run-automaton-core.sh
+++ build-and-run-automaton-core.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 cd src
 mkdir -p logs
-bazel build //automaton/core && bazel-bin/automaton/core/core 2>/dev/null
+bazel build -c opt //automaton/core && bazel-bin/automaton/core/core 2>/dev/null
 reset
 
