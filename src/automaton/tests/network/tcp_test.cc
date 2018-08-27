311072c01607485694f0aa2bc4a7728c288b539e
liberatix-kari <you@example.com>
2018-08-27 13:18:59 +0300

    Tcp test updated

--- src/automaton/tests/network/tcp_test.cc
+++ src/automaton/tests/network/tcp_test.cc
@@ -10,42 +10,51 @@ using automaton::core::network::connection;
 
 const char* address_a = "127.0.0.1:12333";
 const char* address_b = "127.0.0.1:12366";
+
+std::map<uint32_t, automaton::core::network::connection*> connections;
+
 int counter = 100;
-// bool passed = true;
+static uint32_t cids = 0;
+std::mutex ids_mutex;
+
+uint32_t get_new_id() {
+  std::lock_guard<std::mutex> lock(ids_mutex);
+  return ++cids;
+}
+
 char* bufferA = new char[256];
 char* bufferB = new char[256];
 char* bufferC = new char[256];
 
 class handler: public connection::connection_handler {
  public:
-  void on_message_received(connection* c, char* buffer, uint32_t bytes_read, uint32_t id) {
+  void on_message_received(uint32_t c, char* buffer, uint32_t bytes_read, uint32_t mid) {
     std::string message = std::string(buffer, bytes_read);
-    LOG(INFO) << "Message \"" << message << "\" received from " << c->get_address();
+    LOG(INFO) << "Message \"" << message << "\" received from " << c;
     if (message.compare("Thank you!")) {
-      c->async_send("Thank you!", counter++);
+      connections[c]->async_send("Thank you!", counter++);
     }
-    c -> async_read(buffer, 256, 0);
+    connections[c]->async_read(buffer, 256, 0);
   }
-  void on_message_sent(connection* c, uint32_t id, connection::error e) {
+  void on_message_sent(uint32_t c, uint32_t mid, connection::error e) {
     if (e) {
-      LOG(INFO) << "Message with id " << std::to_string(id) << " was NOT sent to " <<
-          c->get_address() << "\nError " << std::to_string(e) << " occured";
+      LOG(INFO) << "Message with id " << std::to_string(mid) << " was NOT sent to " <<
+          c << "\nError " << std::to_string(e) << " occured";
     } else {
-      LOG(INFO) << "Message with id " << std::to_string(id) <<
-          " was successfully sent to " << c->get_address();
+      LOG(INFO) << "Message with id " << std::to_string(mid) << " was successfully sent to " << c;
     }
   }
-  void on_connected(connection* c) {
-    LOG(INFO) << "Connected with: " + c->get_address();
+  void on_connected(uint32_t c) {
+    LOG(INFO) << "Connected with: " + c;
   }
-  void on_disconnected(connection* c) {
-    LOG(INFO) << "Disconnected with: " + c->get_address();
+  void on_disconnected(uint32_t c) {
+    LOG(INFO) << "Disconnected with: " + c;
   }
-  void on_error(connection* c, connection::error e) {
+  void on_error(uint32_t c, connection::error e) {
     if (e == connection::no_error) {
       return;
     }
-    LOG(ERROR) << std::to_string(e) << " (connection " << c->get_address() << ")";
+    LOG(ERROR) << std::to_string(e) << " (connection " << c << ")";
   }
 };
 
@@ -53,13 +62,15 @@ class lis_handler: public acceptor::acceptor_handler {
  public:
   // TODO(kari): Add constructor that accepts needed options
   // (vector connections, max ...)
-  bool on_requested(acceptor* a, const std::string& address) {
+  bool on_requested(acceptor* a, const std::string& address, uint32_t* pid) {
   //  EXPECT_EQ(address, address_a);
+    *pid = get_new_id();
     LOG(INFO) << "Connection request from: " << address << ". Accepting...";
     return true;
   }
   void on_connected(acceptor* a, connection* c, const std::string& address) {
     LOG(INFO) << "Accepted connection from: " << address;
+    connections[c->get_id()] = c;
     char* buffer = new char[256];
     c->async_read(buffer, 256, 0);
   }
@@ -68,39 +79,12 @@ class lis_handler: public acceptor::acceptor_handler {
   }
 };
 
-handler handlerA, handlerB, handlerC, handlerD;
-// void thread1() {
-//   connection* connection_a = connection::create("tcp", address_b, &handlerA);
-//   connection_a->init();
-//   connection_a -> connect();
-//   connection_a -> async_read(bufferA, 256, 0);
-//   connection_a -> async_send("A0", 1);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(300));
-//   /*
-//   connection_a -> async_send("A1", counter++);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(200));
-//   connection_a -> async_send("A2", counter++);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-//   */
-//   connection_a->disconnect();
-// }
-// void thread2() {
-//   connection* connection_b = connection::create("tcp", address_b, &handlerB);
-//   connection_b->init();
-//   connection_b -> connect();
-//   connection_b -> async_read(bufferB, 256, 0);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(60));
-//   connection_b -> async_send("B0", 2);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//   // connection_b -> async_send("B1", counter++);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(50));
-//   // connection_b -> async_send("B2", counter++);
-//   connection_b -> disconnect();
-//   // connection_b -> async_send("B2", counter++);  // Error
-// }
-void thread3() {
-  connection* connection_c = connection::create("tcp", address_b, &handlerC);
+handler handlerC, handlerA;
+
+void func() {
+  connection* connection_c = connection::create("tcp", get_new_id(), address_a, &handlerC);
   if (connection_c->init()) {
+    connections[connection_c->get_id()] = connection_c;
     LOG(DEBUG) << "Connection init was successful!";
     connection_c -> connect();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -130,23 +114,17 @@ void thread3() {
 int main() {
   automaton::core::network::tcp_init();
   lis_handler lis_handler;
-  acceptor* acceptorB = acceptor::create("tcp", address_b, &lis_handler,
-      &handlerD);
+  acceptor* acceptorB = acceptor::create("tcp", address_a, &lis_handler, &handlerA);
   if (acceptorB->init()) {
     LOG(DEBUG) << "Acceptor init was successful!";
     acceptorB->start_accepting();
   } else {
     LOG(ERROR) << "Acceptor init failed!";
   }
-  //  std::thread t1(thread1);
-  //  std::thread t2(thread2);
-  std::thread t3(thread3);
-  /*if (!passed) {
-    return 1;
-  }*/
+  std::thread t(func);
+
   char r; std::cin >> r;
-  //  t1.join();
-  //  t2.join();
-  t3.join();
+  t.join();
+  automaton::core::network::tcp_release();
   return 0;
 }
