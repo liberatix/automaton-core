b41720e283c26a6c61f5b5d8bfafc74e03b01530
liberatix-samir <Samir>
2018-09-11 20:00:48 +0300

    secp256k1 ecdsa functions

--- src/automaton/core/script/bind_crypto.cc
+++ src/automaton/core/script/bind_crypto.cc
@@ -2,11 +2,13 @@
 
 #include "automaton/core/crypto/cryptopp/Keccak_256_cryptopp.h"
 #include "automaton/core/crypto/cryptopp/RIPEMD160_cryptopp.h"
+#include "automaton/core/crypto/cryptopp/secp256k1_cryptopp.h"
 #include "automaton/core/crypto/cryptopp/secure_random_cryptopp.h"
 #include "automaton/core/crypto/cryptopp/SHA256_cryptopp.h"
 #include "automaton/core/crypto/cryptopp/SHA3_256_cryptopp.h"
 #include "automaton/core/crypto/cryptopp/SHA512_cryptopp.h"
 #include "automaton/core/crypto/ed25519_orlp/ed25519_orlp.h"
+#include "automaton/core/io/io.h"
 
 using automaton::core::crypto::cryptopp::Keccak_256_cryptopp;
 using automaton::core::crypto::cryptopp::RIPEMD160_cryptopp;
@@ -14,6 +16,8 @@ using automaton::core::crypto::cryptopp::secure_random_cryptopp;
 using automaton::core::crypto::cryptopp::SHA256_cryptopp;
 using automaton::core::crypto::cryptopp::SHA512_cryptopp;
 using automaton::core::crypto::cryptopp::SHA3_256_cryptopp;
+using automaton::core::crypto::cryptopp::secp256k1_cryptopp;
+using automaton::core::io::hex2bin;
 
 namespace automaton {
 namespace core {
@@ -25,6 +29,7 @@ void engine::bind_crypto() {
   sha256.reset(new SHA256_cryptopp());
   sha3.reset(new SHA3_256_cryptopp());
   keccak256.reset(new Keccak_256_cryptopp());
+  secp256k1.reset(new secp256k1_cryptopp());
 
   set_function("ripemd160", [&](const std::string& s) -> std::string {
     uint8_t digest[20];
@@ -54,6 +59,30 @@ void engine::bind_crypto() {
     uint8_t digest[32];
     keccak256->calculate_digest(reinterpret_cast<const uint8_t*>(s.data()), s.size(), digest);
     return std::string((char*)digest, 32); // NOLINT
+  });
+
+  // ECDSA functions
+  set_function("secp256k1_sign", [&](const std::string& pr_key, const std::string& msg) -> std::string {
+    uint8_t signature[64];
+    secp256k1->sign(reinterpret_cast<const uint8_t*>(hex2bin(pr_key).data()),
+                    reinterpret_cast<const uint8_t*>(msg.data()),
+                    msg.size(),
+                    signature);
+    return std::string((char*)signature, secp256k1->signature_size()); // NOLINT
+  });
+
+  set_function("secp256k1_gen_public_key", [&](const std::string& pr_key) -> std::string {
+    uint8_t public_key[33];
+    secp256k1->gen_public_key(reinterpret_cast<const uint8_t*>(hex2bin(pr_key).data()), public_key);
+    return std::string((char*)public_key, secp256k1->public_key_size()); // NOLINT
+  });
+  // TODO(Samir): signature should be const
+  set_function("secp256k1_verify", [&](const std::string& pub_key,const std::string& msg,
+              std::string& signature) -> bool {
+    return secp256k1->verify(reinterpret_cast<const uint8_t*>(hex2bin(pub_key).data()),
+                             reinterpret_cast<const uint8_t*>(msg.data()),
+                             msg.size(),
+                             reinterpret_cast<uint8_t*>(&(signature[0]))); // TODO(Samir): Fix the const away casting
   });
 }
 
