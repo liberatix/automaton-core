0de2f1360936ff9253a6de5f89f234752cebf8f3
liberatix-samir <Samir>
2018-09-13 19:33:47 +0300

    Cleanup

--- src/automaton/examples/smartproto/reservationsystem/reservationsystem.lua
+++ src/automaton/examples/smartproto/reservationsystem/reservationsystem.lua
@@ -1,27 +1,7 @@
--- reservationsystem.lua
-
--- create reservtion timer
-cr_timer = math.random(100, 1000)
-
 function update(time)
   if table_length(validators) == TOTAL_VALIDATORS then
-    update_state(time)
+    update_sate(time)
   end
-
-  cr_timer = cr_timer - 1
-  if cr_timer <= 0 then
-    create_random_reservation()
-    cr_timer = math.random(200, 1000)
-  end
-end
-
-function create_random_reservation()
-  tx = create_tx("create",
-      { math.random(1, ROOM_COUNT) },
-      math.random(1, DAYS / 2),
-      math.random(DAYS / 2 + 1, DAYS),
-      public_key)
-  on_CreateReservation(0, tx)
 end
 
 function sent(peer_id, msg_id, success)
@@ -66,10 +46,10 @@ end
 -- }
 
 function on_StateTransition(peer_id, state_transition)
-
+  -- validate state_transition
+  -- apply changes
+  -- broadcast(peer_id, state_transition)
 end
-
-msg_hash = {}
 
 function on_CreateReservation(peer_id, reservation)
   if not is_valid_signature(reservation) then
@@ -93,13 +73,9 @@ function on_CreateReservation(peer_id, reservation)
   --   end
   -- end
 
-  hash = sha3(reservation:serialize())
-  if not msg_hash[hash] then
-    msg_hash[hash] = true
-    log("on_CreateReservation", tostring(peer_id) .. reservation:to_json())
-    table.insert(pending_reservations, reservation)
-    gossip(peer_id, reservation)
-  end
+
+  table.insert(pending_reservations, reservation)
+  gossip(peer_id, reservation)
 end
 
 function on_CancelReservation(peer_id, cancellation)
@@ -123,12 +99,8 @@ function on_CancelReservation(peer_id, cancellation)
   --   end
   -- end
 
-  hash = sha3(cancellation:serialize())
-  if not msg_hash[hash] then
-    msg_hash[hash] = true
-    table.insert(pending_cancellations, cancellation)
-    gossip(peer_id, cancellation)
-  end
+  table.insert(pending_cancellations, cancellation)
+  gossip(peer_id, cancellation)
 end
 
 function create_tx(tx_type, rooms, start_day, end_day, client_public_key)
