c8c8b42921af695ca6f1efa45ea6e9cfd1d082e3
liberatix-ace <36509087+liberatix-ace@users.noreply.github.com>
2018-09-14 15:38:56 +0300

    Refactor - separate logic in more files (#371)

--- src/automaton/examples/smartproto/reservationsystem/connections.lua
+++ src/automaton/examples/smartproto/reservationsystem/connections.lua
@@ -1,6 +1,6 @@
 current_message_id = 1
 function connected(peer_id)
-  peers[peer_id] = { status = "connected" }
+  peers[peer_id] = { status = "connected", name = "N/A" .. tostring(peer_id) }
   send(peer_id, validators_msg(), current_message_id)
   current_message_id = current_message_id + 1
 end
@@ -11,17 +11,9 @@ end
 
 function gossip(peer_id, msg)
   for k,v in pairs(peers) do
-    if k ~= peer_id and v.status == "connected" then
+    if k ~= 0 and k ~= peer_id and v.status == "connected" then
       send(k, msg, current_message_id)
       current_message_id = current_message_id + 1
     end
   end
-end
-
-function validators_msg()
-  local m = RegisterValidators()
-  for k in pairs(validators) do
-    m.public_key = k
-  end
-  return m
 end
--- src/automaton/examples/smartproto/reservationsystem/reservationsystem.lua
+++ src/automaton/examples/smartproto/reservationsystem/reservationsystem.lua
@@ -29,45 +29,9 @@ end
 function sent(peer_id, msg_id, success)
 end
 
-function on_RegisterValidators(peer_id, msg)
-  log("on_RegisterValidators", "call by peer: " .. tostring(peer_id))
-  local new_validator = false
-  for k,v in pairs(msg.public_key) do
-    if validators[v] == nil then
-      log("new_validator_from ", nodeid .. " RECEIVED " .. hex(v))
-      validators[v] = 1
-      new_validator = true
-    end
-  end
-  if new_validator then
-    gossip(peer_id, validators_msg())
-    if table_length(validators) == TOTAL_VALIDATORS then
-      for k, v in pairs(validators) do
-        table.insert(validators_sorted, k)
-      end
-      table.sort(validators_sorted)
-      --log("total_validators ", validators_sorted)
-      for i,n in ipairs(validators_sorted) do
-        log("validators", tostring(i) .. ": " .. hex(n))
-        if public_key == n then
-          our_slot = i
-          log("validators", tostring(i) .. " IS OUR SLOT!")
-        end
-      end
-    end
-  end
-end
--- essage StateTransition {
---   //  validator_number * rounds
---   uint64 epoch = 1;
---   repeated CreateReservation reservations = 2;
---   repeated CancelReservation cancellations = 3;
---   // sign(epoch, reservations, cancellations) with the key of the validator
---   bytes signature = 4;
--- }
 function on_StateTransition(peer_id, state_transition)
-  log("on_StateTransition", "peer_id:" .. tostring(peer_id)) -- log the state transition
-  log("on_StateTransition", log_StateTransition(state_transition)) -- log the state transition
+  log("on_StateTransition", "SENT FROM: " .. peers[peer_id].name .. "\n"
+    .. log_StateTransition(state_transition)) -- log the state transition
   if valid_transition(state_transition) then
     log("on_StateTransition", "VALID!")
     apply_transition(state_transition)
@@ -142,7 +106,6 @@ function valid_transition(st)
   local signature = st.signature
   st.signature = ""
   local serialized = st:serialize()
-  log("signing", "serialized: " .. hex(serialized))
   local state_valid = secp256k1_verify(validators_sorted[epoch % TOTAL_VALIDATORS + 1], serialized, signature)
   st.signature = signature
 
@@ -231,122 +194,6 @@ function create_tx(tx_type, rooms, start_day, end_day, client_public_key)
   to_sign = to_sign .. tostring(start_day) .. tostring(end_day)
   msg.client_signature = secp256k1_sign(private_key, to_sign)
   return msg
-end
-
-function debug_html()
-  local html = ""
-  for i = 1, ROOM_COUNT do
-    html = html .. get_room_html(i)
-  end
-  html = html .. [[
-  <div id="noteID", class="note">Tooltip text</div>
-  <br />
-  <style>
-  .busy {
-  	margin: 10px;
-      padding: 5px;
-      background: #d5c7d5;
-      color: black
-  }
-  .selected {
-  	margin: 10px;
-      padding: 5px;
-      background: #58b7bf;
-      color: black
-  }
-  table {
-  	width: 300px;
-    height: 300px;
-    font-family: Verdana, sans-serif;
-    font-size: 18px;
-    background: #edf6f6;
-    align: left;
-    text-align: center;
-    border: "0";
-    margin: 16px;
-    cursor: pointer;
-  }
-  caption {
-  	align: "center";
-      text-align: center;
-      font-size: 25px;
-      border: "0";
-      margin: 0;
-      background: #7dc2c8;
-      cursor: default;
-  }
-  td {
-  	margin: 10px;
-      padding: 5px;
-  }
-  .note {
-  	visibility: hidden;
-      font-family: Verdana, sans-serif;
-      font-size: 18px;
-      background: #edf6f6;
-      align: right;
-      text-align: center;
-      border: "0";
-      margin: 0;
-      position: absolute;
-      top: 150px;
-      left: 35%;
-  }
-  </style>
-  <script>
-  var tables = document.getElementsByClassName("calendar");
-  for (var k = 0; k < tables.length; k++) {
-    var table = tables[k]
-  	for (var i = 0; i < table.rows.length; i++) {
-          for (var j = 0; j < table.rows[i].cells.length; j++) {
-  			table.rows[i].cells[j].onmouseover = function () {
-  				on_selection(this, k, true);
-  			};
-              table.rows[i].cells[j].onmouseleave = function () {
-  				on_selection(this, k, false);
-  			};
-          }
-  	}
-  }
-
-  function on_selection(table_cell, table_number, over) {
-  	var current = document.getElementsByClassName("selected");
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" selected", "");
-      }
-      var note = document.getElementById("noteID");
-      if(over) {
-      	table_cell.className += " selected";
-          note.innerHTML = table_cell.id;
-      	note.style = "visibility:visible";
-      }
-      else {
-      	note.style = "visibility:hidden";
-      }
-  }
-  </script>
-  ]]
-  return html
-end
-
-function get_room_html(room_number)
-  local table_html = [[
-  <table class="calendar">
-    <caption>ROOM ]] .. tostring(room_number) .. [[</caption>
-    <tr>
-  ]]
-  for i = 1, DAYS do
-    if (i - 1) % 5 == 0 then
-      table_html = table_html .. "</tr><tr>"
-    end
-    if rooms[room_number][i] then
-      table_html = table_html .. "<td class=\"busy\" id=" .. hex(rooms[room_number][i]) .. ">" .. tostring(i) .. "</td>"
-    else
-      table_html = table_html .. "<td>" .. tostring(i) .. "</td>"
-    end
-  end
-  table_html = table_html .. "</tr></table>"
-  return table_html
 end
 
 function tests()
