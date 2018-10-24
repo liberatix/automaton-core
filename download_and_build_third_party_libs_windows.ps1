f7bd52fdd044a6a0d0aa489494d59187852a07ec
liberatix-samir <Samir>
2018-10-24 18:43:25 +0300

    Update download_and_build_third_party_libs_windows.ps1

--- download_and_build_third_party_libs_windows.ps1
+++ download_and_build_third_party_libs_windows.ps1
@@ -1,4 +1,9 @@
-﻿cd D:\GitHub\automaton-core\ # Remove this line before commit 
+﻿# To run this script: 
+# 1. Stat Developer Command Prompt for VS 
+# 2. Type powershell
+# 3. Enter the script directory and run it
+
+cd D:\GitHub\automaton-core\ # Remove this line before commit 
 
 # Get path to VS, remember to start powershell if using command prompt
 #$key = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
@@ -9,7 +14,7 @@
 
 $LOCAL_3P="local_third_party"
 cd .\src\
-New-Item -ItemType Directory -force -Path .\$LOCAL_3P
+New-Item -ItemType Directory -Path .\$LOCAL_3P
 cd .\$LOCAL_3P
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11"
 
@@ -35,41 +40,40 @@ function Get-GitRepo($repo, $dir, $commit) {
  
 }
 
-#function get_archive() {
-#  url=$1
-#  filename=$2
-#  sha=$3
-#
-#  print_separator "=" 80
-#  echo "  Downloading $filename"
-#  print_separator "=" 80
-#
-#  [ ! -f $2 ] && wget $1
-#
-#  filesha=$(shasum -a 256 $filename | cut -d' ' -f1)
-#  [ $filesha == $sha ] || ( echo "Error: Wrong hash [$filesha] Expected [$sha]" && exit 1 )
-#
-#  echo "Extracting $filename"
-#  tar -xzf $filename
-#}
+function Get-Archive($url, $filename, $sha) {
+  echo ("="*80)
+  echo "  Downloading $filename"
+  echo ("="*80)
+  if (!(Test-Path .\$filename)) {
+    wget -URI $url -OutFile $filename
+  }
+  $filehash = (Get-FileHash -Path .\$filename -Algorithm SHA256).Hash
+  if($filehash -ne $sha) {
+    echo "Error: Wrong hash [$filehash] Expected [$sha]"
+  } else {
+    echo "Extracting $filename"
+    Expand-Archive -Path $filename -DestinationPath .\ -Force
+    echo "done"
+  }
+}
 
 
 #  ========  Download all libraries ========
 Get-GitRepo "https://github.com/LuaJIT/LuaJIT.git" "LuaJIT" "0bf80b07b0672ce874feedcc777afe1b791ccb5a"
-#Get-GitRepo "https://github.com/zeromq/libzmq.git" "libzmq" "d062edd8c142384792955796329baf1e5a3377cd"
-#Get-GitRepo "https://github.com/zeromq/cppzmq.git" "cppzmq" "d9f0f016c07046742738c65e1eb84722ae32d7d4"
-#Get-GitRepo "https://github.com/zeromq/zmqpp.git" "zmqpp" "f8ff127683dc555aa004c0e6e2b18d2354a375be"
-#Get-GitRepo "https://github.com/ThePhD/sol2.git" "sol2" "254466eb4b3ae630c731a557987f3adb1a8f86b0"
-#Get-GitRepo "https://github.com/AmokHuginnsson/replxx.git" "replxx" "3cb884e3fb4b1a28efeb716fac75f77eecc7ea3d"
+Get-GitRepo "https://github.com/zeromq/libzmq.git" "libzmq" "d062edd8c142384792955796329baf1e5a3377cd"
+Get-GitRepo "https://github.com/zeromq/cppzmq.git" "cppzmq" "d9f0f016c07046742738c65e1eb84722ae32d7d4"
+Get-GitRepo "https://github.com/zeromq/zmqpp.git" "zmqpp" "f8ff127683dc555aa004c0e6e2b18d2354a375be"
+Get-GitRepo "https://github.com/ThePhD/sol2.git" "sol2" "254466eb4b3ae630c731a557987f3adb1a8f86b0"
+Get-GitRepo "https://github.com/AmokHuginnsson/replxx.git" "replxx" "3cb884e3fb4b1a28efeb716fac75f77eecc7ea3d"
 Get-GitRepo "https://github.com/lua/lua.git" "lua" "e354c6355e7f48e087678ec49e340ca0696725b1"
-#Get-GitRepo "https://github.com/muflihun/easyloggingpp.git" "easyloggingpp" "a5317986d74b6dd3956021cb7fbb0669cce398b2"
-#Get-GitRepo "https://github.com/weidai11/cryptopp.git" "cryptopp" "c8d8caf70074655a2562ae1ea45cb30e28fee2b4"
-#Get-GitRepo "https://github.com/orlp/ed25519.git" "ed25519" "7fa6712ef5d581a6981ec2b08ee623314cd1d1c4"
-#Get-GitRepo "https://github.com/google/googletest.git" "googletest" "2fe3bd994b3189899d93f1d5a881e725e046fdc2"
-## Get-GitRepo "https://github.com/nlohmann/json.git" "json" "359f98d14065bf4e53eeb274f5987fd08f16e5bf"
-#Get-GitRepo "https://github.com/nelhage/rules_boost.git" "com_github_nelhage_rules_boost" "fe787183c14f2a5c6e5e1e75a7c57d2e799d3d19"
-#Get-GitRepo "https://github.com/protocolbuffers/protobuf.git" "protobuf" "48cb18e5c419ddd23d9badcfe4e9df7bde1979b2"
-#Get-GitRepo "https://github.com/svaarala/duktape.git" "duktape" "d7fdb67f18561a50e06bafd196c6b423af9ad6fe"
+Get-GitRepo "https://github.com/muflihun/easyloggingpp.git" "easyloggingpp" "a5317986d74b6dd3956021cb7fbb0669cce398b2"
+Get-GitRepo "https://github.com/weidai11/cryptopp.git" "cryptopp" "c8d8caf70074655a2562ae1ea45cb30e28fee2b4"
+Get-GitRepo "https://github.com/orlp/ed25519.git" "ed25519" "7fa6712ef5d581a6981ec2b08ee623314cd1d1c4"
+Get-GitRepo "https://github.com/google/googletest.git" "googletest" "2fe3bd994b3189899d93f1d5a881e725e046fdc2"
+# Get-GitRepo "https://github.com/nlohmann/json.git" "json" "359f98d14065bf4e53eeb274f5987fd08f16e5bf"
+Get-GitRepo "https://github.com/nelhage/rules_boost.git" "com_github_nelhage_rules_boost" "fe787183c14f2a5c6e5e1e75a7c57d2e799d3d19"
+Get-GitRepo "https://github.com/protocolbuffers/protobuf.git" "protobuf" "48cb18e5c419ddd23d9badcfe4e9df7bde1979b2"
+Get-GitRepo "https://github.com/svaarala/duktape.git" "duktape" "d7fdb67f18561a50e06bafd196c6b423af9ad6fe"
 
 
 
@@ -79,6 +83,27 @@ if(!(Test-Path -Path .\json-3.1.2) -and (New-Item -ItemType Directory -Path .\js
   wget -URI https://github.com/nlohmann/json/releases/download/v3.2.0/json.hpp -OutFile json-3.1.2/json.hpp
 }
 
+
+
+if(!(Test-Path -Path .\boost_1_68_0)) {
+    Get-Archive "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip" `
+  "boost_1_68_0.zip" "3B1DB0B67079266C40B98329D85916E910BBADFC3DB3E860C049056788D4D5CD"
+}
+
+if(!(Test-Path -Path .\zlib-1.2.11)) {
+    Get-Archive "https://zlib.net/zlib1211.zip" `
+  "zlib-1.2.11.zip" "d7510a8ee1918b7d0cad197a089c0a2cd4d6df05fee22389f67f115e738b178d"
+}
+
+if(!(Test-Path -Path .\bzip2-1.0.6)) {
+    Get-Archive "https://fossies.org/linux/misc/bzip2-1.0.6.zip" `
+  "bzip2-1.0.6.zip" "C60C0ABC89A76C170B3EF39E7C516DBDEA806D65231204C1803A610FA9DBC464"
+}
+
+#if(!(Test-Path -Path .\xz-5.2.3)) {
+#    Get-Archive "http://phoenixnap.dl.sourceforge.net/project/lzmautils/xz-5.2.3.tar.gz" `
+#  "xz-5.2.3.tar.gz" "71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb"
+#}
 
 
 
@@ -98,14 +123,63 @@ $lua_extern_includes | Out-File -FilePath .\lua\lua.hpp -Encoding utf8
 
 #  ====== Building LuaJIT ======
 echo ("="*80)
-echo "  Create lua.hpp"
+echo "  BUILDING LuaJIT"
 echo ("="*80)
 
 cd LuaJIT\src
 .\msvcbuild.bat
 cd ..\..
 
+#  ====== Building libzmq ======
+echo ("="*80)
+echo "  BUILDING libzmq"
+echo ("="*80)
+
+#print_separator "=" 80
+#echo "   BUILDING libzmq"
+#print_separator "=" 80
+#
+#unset GREP_COLOR
+#unset GREP_OPTIONS
+#cd libzmq
+#[ ! -f configure ] && ./autogen.sh && ./configure
+#make
+#cd ..
+
+#  ====== Building zmqpp ======
+#echo ("="*80)
+#echo "  BUILDING zmqpp"
+#echo ("="*80)
+
+#cd zmqpp
+#$sedi 's/CUSTOM_INCLUDE_PATH =/CUSTOM_INCLUDE_PATH = -I..\/libzmq\/include/' Makefile
+#$sedi 's/LIBRARY_LIBS =/LIBRARY_LIBS = -L..\/libzmq\/src\/.libs/' Makefile
+#make
+#cd ..
+
+#  ====== Building replxx ======
+echo ("="*80)
+echo "  BUILDING replxx"
+echo ("="*80)
+
+cd replxx
+New-Item -ItemType Directory -Path .\build
+cd build
+if (!(Test-Path .\CMakeCache.txt)) {
+    cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..
+}
+msbuild.exe replxx.sln
+cd ..\..
 
 
+#  ====== Building boost ======
+echo ("="*80)
+echo "  BUILDING boost"
+echo ("="*80)
+
+cd boost_1_68_0
+bootstrap.bat
+b2 cxxstd=14 link=static runtime-link=static stage
+cd ..
 
 cd D:\GitHub\automaton-core\ # Remove this line before commit 
