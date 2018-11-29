4a282c6b93e459004fbee16aa56b21e0d5fdd5aa
liberatix-kari <you@example.com>
2018-11-29 14:11:04 +0200

    commands added to chat node

--- src/automaton/examples/smartproto/chat/init.lua
+++ src/automaton/examples/smartproto/chat/init.lua
@@ -1,6 +1,9 @@
 -- chat init.lua
 history_add("testnet(localhost, chat_node, 5, 1, \"logs/chat/\")")
--- testnet(localhost, chat_node, 5, 1, "automaton/examples/smartproto/chat/")
+history_add("nodes = get_nodes(\"logs/chat/\")")
+history_add("p = nodes[2]:process_cmd(\"get_peers\", \"\")")
+history_add("m = nodes[2]:process_cmd(\"get_messages\", \"\")")
+
 function chat_node(id)
   local n = node(id, "automaton/examples/smartproto/chat/")
 
